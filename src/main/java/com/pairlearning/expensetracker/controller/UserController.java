a8e21b3491682f5c1261ad05e16e757c72a0d061
shraddha atrawalkar <shraddha.atrawalkar@coxautoinc.com>
2020-08-16 21:26:24 -0400

    update readme

--- src/main/java/com/pairlearning/expensetracker/controller/UserController.java
+++ src/main/java/com/pairlearning/expensetracker/controller/UserController.java
@@ -8,10 +8,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -23,7 +20,7 @@ public class UserResource {
 
     @Autowired
     UserService userService;
-
+    @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap) {
         String email = (String) userMap.get("email");
